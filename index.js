dc26bbf04213aed1e4f7d1503435344d087a79a8
No1-man-Oira <getoubashi@gmail.com>
2019-09-15 05:27:55 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function getImg(stampId, itemNum) {
+    const result = document.getElementsByClassName('result')[0];
+    result.innerHTML = '';
+
+    for (let i = 0; i < itemNum; i++) {
+        const img = window.document.createElement('img');
+        const src = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${stampId + i}/android/sticker.png;compress=true`;
+        img.setAttribute('src', src);
+        result.appendChild(img);
+    }
+}
