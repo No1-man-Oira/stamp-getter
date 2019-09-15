7711ca9c44606fe31aada9a2cdc06f41a5e80fd7
No1-man-Oira <getoubashi@gmail.com>
2019-09-15 16:53:32 +0900

    update サイズ変更

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function getImg(stampId, itemNum) {
 
     for (let i = 0; i < itemNum; i++) {
         const img = window.document.createElement('img');
-        const src = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${stampId + i}/android/sticker.png;compress=true`;
+        const src = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${stampId + i}/iPhone/sticker_key@2x.png;compress=true`;
         img.setAttribute('src', src);
         result.appendChild(img);
     }
